7d386aac57a12f8213d2784f11fb6e2b0282ece7
Suman Mondal <sumanmondalmb71@gmail.com>
2023-05-05 15:00:21 +0530

    building 5-5-23 3:00pm

--- src/myApp/urls.py
+++ src/myApp/urls.py
@@ -3,20 +3,51 @@ from django.urls import path
 from myApp import views
 
 urlpatterns = [
+    # Navbar Section
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
     path("products", views.products, name='products'),
     path("services", views.services, name='services'),
     path("contact", views.contact, name='contact'),
+    path("profile", views.profile, name='profile'),
+    path("cart", views.cart, name='cart'),
+
+    # Product Section
     path("laptops", views.laptops, name='laptops'),
     path("mice", views.mice, name='mice'),
     path("keyboards", views.keyboards, name='keyboards'),
     path("headphones", views.headphones, name='headphones'),
     path("monitors", views.monitors, name='monitors'),
     
+    # Laptop Section
     path("laptop+model+1", views.lt_model_1, name='laptop+model+1'),
     path("laptop+model+2", views.lt_model_2, name='laptop+model+2'),
     path("laptop+model+3", views.lt_model_3, name='laptop+model+3'),
     path("laptop+model+4", views.lt_model_4, name='laptop+model+4'),
+
+    # Mice Section
+    path("mice+model+1", views.mc_model_1, name='mice+model+1'),
+    path("mice+model+2", views.mc_model_2, name='mice+model+2'),
+    path("mice+model+3", views.mc_model_3, name='mice+model+3'),
+    path("mice+model+4", views.mc_model_4, name='mice+model+4'),
+
+    # Keyboard Section
+    path("keyboards+model+1", views.kb_model_1, name='keyboards+model+1'),
+    path("keyboards+model+2", views.kb_model_2, name='keyboards+model+2'),
+    path("keyboards+model+3", views.kb_model_3, name='keyboards+model+3'),
+    path("keyboards+model+4", views.kb_model_4, name='keyboards+model+4'),
+
+    # Headphone Section
+    path("headphones+model+1", views.hp_model_1, name='headphones+model+1'),
+    path("headphones+model+2", views.hp_model_2, name='headphones+model+2'),
+    path("headphones+model+3", views.hp_model_3, name='headphones+model+3'),
+    path("headphones+model+4", views.hp_model_4, name='headphones+model+4'),
+
+    # Monitor Section
+    path("monitors+model+1", views.mt_model_1, name='monitors+model+1'),
+    path("monitors+model+2", views.mt_model_2, name='monitors+model+2'),
+    path("monitors+model+3", views.mt_model_3, name='monitors+model+3'),
+    path("monitors+model+4", views.mt_model_4, name='monitors+model+4'),
+    
 ]
 
--- src/myApp/views.py
+++ src/myApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-
+# Navbar section 
 def index(request):
     return render(request, "pages/home.html", {})
 
@@ -16,8 +16,14 @@ def services(request):
 def contact(request):
     return render(request, "pages/contact.html", {})
 
+def profile(request):
+    return render(request, "pages/profile.html", {})
+
+def cart(request):
+    return render(request, "pages/cart.html", {})
 
 
+# Product Section 
 def laptops(request):
     return render(request, "pages/product_pages/laptops.html", {})
 
@@ -34,7 +40,7 @@ def monitors(request):
     return render(request, "pages/product_pages/monitors.html", {})
 
 
-
+# Laptop Section 
 def lt_model_1(request):
     return render(request, "pages/product_pages/laptop_pages/lt-model1.html", {})
 
@@ -46,3 +52,58 @@ def lt_model_3(request):
 
 def lt_model_4(request):
     return render(request, "pages/product_pages/laptop_pages/lt-model4.html", {})
+
+
+# Mice Section 
+def mc_model_1(request):
+    return render(request, "pages/product_pages/Mice_pages/mc-model1.html", {})
+
+def mc_model_2(request):
+    return render(request, "pages/product_pages/Mice_pages/mc-model2.html", {})
+
+def mc_model_3(request):
+    return render(request, "pages/product_pages/Mice_pages/mc-model3.html", {})
+
+def mc_model_4(request):
+    return render(request, "pages/product_pages/Mice_pages/mc-model4.html", {})
+
+# Keyboard Section
+def kb_model_1(request):
+    return render(request, "pages/product_pages/keyboard_pages/kb-model1.html", {})
+
+def kb_model_2(request):
+    return render(request, "pages/product_pages/keyboard_pages/kb-model2.html", {})
+
+def kb_model_3(request):
+    return render(request, "pages/product_pages/keyboard_pages/kb-model3.html", {})
+
+def kb_model_4(request):
+    return render(request, "pages/product_pages/keyboard_pages/kb-model4.html", {})
+
+
+# headphones Section
+def hp_model_1(request):
+    return render(request, "pages/product_pages/headphone_pages/hp-model1.html", {})
+
+def hp_model_2(request):
+    return render(request, "pages/product_pages/headphone_pages/hp-model2.html", {})
+
+def hp_model_3(request):
+    return render(request, "pages/product_pages/headphone_pages/hp-model3.html", {})
+
+def hp_model_4(request):
+    return render(request, "pages/product_pages/headphone_pages/hp-model4.html", {})
+
+
+# monitors Section 
+def mt_model_1(request):
+    return render(request, "pages/product_pages/monitor_pages/mt-model1.html", {})
+
+def mt_model_2(request):
+    return render(request, "pages/product_pages/monitor_pages/mt-model2.html", {})
+
+def mt_model_3(request):
+    return render(request, "pages/product_pages/monitor_pages/mt-model3.html", {})
+
+def mt_model_4(request):
+    return render(request, "pages/product_pages/monitor_pages/mt-model4.html", {})
