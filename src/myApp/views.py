c74e4fbd7600b6cbc8fd91a202320ddc31a7b344
Suman Mondal <sumanmondalmb71@gmail.com>
2023-05-09 15:56:48 +0530

    something is done 9-5-23

--- src/myApp/views.py
+++ src/myApp/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
-
-from .forms import SignUpForm, UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-
-
+from .forms import SignUpForm
 
 
 # User Section
@@ -21,25 +16,21 @@ from django.views import generic
 # UN: user2
 # PW: sumanmondal@S72
 
+
+
 def signup(request):
-    form = SignUpForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        form.save()
-
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
         form = SignUpForm()
-
-    context = {
-        "form":form
-    }
-    return render (request, "signup.html", context)
-
-
-# class SignUpView(generic.CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy("login")
-#     template_name = "signup.html"
+    return render(request, 'signup.html', {'form': form})
 
 
 def loginUser(request):
@@ -59,7 +50,7 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    return render(request, "pages/home.html")
+    return redirect("/")
 
 
 # Navbar section 
